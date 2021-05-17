47cfa7dc908758100f6dee24a3c838465573d91e
nuwani-attanayaka <73015740+nuwani-attanayaka@users.noreply.github.com>
2021-05-17 13:37:26 +0530

    the new routes file for Guest profile updating

--- routes/profile.js
+++ routes/profile.js
@@ -3,87 +3,164 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Guest = mongoose.model("Guest");
 const requireLogin = require("../middleware/requireLogin");
+const bcrypt = require("bcryptjs");
+const equals = require("validator/lib/equals");
 
-const { authenticatateJWT } = require("../middleware/authenticator");
-const upload = require("../middleware/multer");
-const profileController = require("../controllers/profile");
-
+/***************************
+  GETTING LOGGEDIN USER 
+***************************/
 router.get("/profile", requireLogin, (req, res) => {
   Guest.find({ _id: req.user._id })
 
-    .then((profile) => {
-      res.json({ profile });
+    .then((user) => {
+      res.json({ user });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
+/*******************************
+  UPDATE AND DELETE PROFILE PIC
+********************************/
+router.put("/updatepic", requireLogin, (req, res) => {
+  Guest.findByIdAndUpdate(
+    req.user._id,
+    { $set: { pic: req.body.pic } },
+    { new: true }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// router.delete("/deletepic/:picId", requireLogin, (req, res) => {
+//   Guest.findOne({ _id: req.user._id }).exec((err, pic) => {
+//     if (err || !pic) {
+//       return res.status(422).json({ error: err });
+//     }
+//     if (pic._id.toString() === req.user._id.toString()) {
+//       pic
+//         .remove()
+//         .then((result) => {
+//           res.json(result);
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//         });
+//     }
+//   });
+// });
+
 /*********************
-    UPDATE PIC
+  UPDATE USERNAME
 *********************/
+router.put("/username", requireLogin, (req, res) => {
+  const { firstname, lastname } = req.body;
 
-// const { createProfile } = require("../controllers/profile");
-// router.post(
-//   "/",
-//   authenticatateJWT,
-//   upload.single("profilePic"),
-//   profileController.create,
-//   profileController.createProfile
-// );
+  if (!firstname && !lastname) {
+    return res.status(422).json();
+  } else {
+    Guest.findByIdAndUpdate(
+      req.user._id,
+      { $set: { firstname: req.body.firstname, lastname: req.body.lastname } },
 
-// router.put("/", async (req, res) => {
-//   await profileController.createProfile(req, res);
-// });
+      {
+        new: true,
+      }
+    ).exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      } else {
+        res.json(result);
+      }
+    });
+  }
+});
 
-// router.post("/createProfile", createProfile);
+/********************
+  UPDATE BIO
+*********************/
+router.put("/bio", requireLogin, (req, res) => {
+  const { bio } = req.body;
 
-// router.put("/createProfile", async (req, res) => {
-//   await profileController.createProfile(req, res);
-// });
+  if (!bio) {
+    return res.status(422).json();
+  }
+  Guest.findByIdAndUpdate(
+    req.user._id,
+    { $set: { bio: req.body.bio } },
 
-// //to update
-// router.put("/username/:userId", async (req, res) => {
-//   await profileController.createProfile(req, res);
-// });
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
 
-const { createProfile } = require("../controllers/profile");
-router.patch("/username", authenticatateJWT, createProfile);
+/********************
+  UPDATE LOCATION
+*********************/
+router.put("/location", requireLogin, (req, res) => {
+  const { location } = req.body;
 
-// router.put("/username/:email", async (req, res) => {
-//   await profileController.createProfile(req, res);
-// });
+  if (!location) {
+    return res.status(422).json();
+  }
+  Guest.findByIdAndUpdate(
+    req.user._id,
+    { $set: { location: req.body.location } },
+
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 /********************
   UPDATE PASSWORD
 *********************/
+router.put("/password", requireLogin, async (req, res) => {
+  const { password, password2 } = req.body;
 
-// router.put("/updatepic", guestAuth, (req, res) => {
-//   Guest.findByIdAndUpdate(
-//     req.user._id,
-//     { $set: { pic: req.body.pic } },
-//     { new: true },
-//     (err, result) => {
-//       if (err) {
-//         return res.status(422).json({ error: "picture can not post" });
-//       }
-//       res.json(result);
-//     }
-//   );
-// });
+  if (!equals(password, password2)) {
+    return res.status(422).json();
+  }
+  // } else if (isLength({ min: 6 })) {
+  //   return res.status(423).json();
+  // }
+  const newPassword = new Guest();
 
-// router.put("/updatepic", guestAuth, (req, res) => {
-//   Guest.findByIdAndUpdate(
-//     req.user._id,
-//     { $set: { pic: req.body.pic } },
-//     { new: true },
-//     (err, result) => {
-//       if (err) {
-//         return res.status(422).json({ error: "picture can not post" });
-//       }
-//       res.json(result);
-//     }
-//   );
-// });
+  const salt = await bcrypt.genSalt(10);
+  newPassword.password = await bcrypt.hash(password, salt);
+  await Guest.findByIdAndUpdate(
+    req.user._id,
+    { $set: { password: newPassword.password } },
+
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 module.exports = router;
