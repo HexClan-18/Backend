d73f0481187b360907f533221e70edac7fb564d5
nuwani-attanayaka <73015740+nuwani-attanayaka@users.noreply.github.com>
2021-05-17 13:33:52 +0530

    added bio,pic,location fields

--- models/Owner.js
+++ models/Owner.js
@@ -18,10 +18,25 @@ const OwnerSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    bio: {
+      type: String,
+      default: "",
+      // required: true,
+    },
+    location: {
+      type: String,
+      default: "",
+      //required: true,
+    },
     role: {
       type: Number,
       default: 2,
       //   1 - admin // 2 - owner // 3 - guest
+    },
+    pic: {
+      type: String,
+      default:
+        "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
     },
   },
   { timestamps: true }
