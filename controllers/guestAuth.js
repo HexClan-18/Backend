898dc0642880b5c2fc94f2c9ea9cc17607ce7025
nuwani-attanayaka <73015740+nuwani-attanayaka@users.noreply.github.com>
2021-05-21 22:49:51 +0530

    added added email verification  functions

--- controllers/guestAuth.js
+++ controllers/guestAuth.js
@@ -1,31 +1,15 @@
 // database validations
 const Guest = require("../models/Guest");
-const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { jwtSecret, jwtExpire } = require("../config/keys");
-const nodemailer = require("nodemailer");
-const sendgridTransport = require("nodemailer-sendgrid-transport");
-
-/************************************
- *SENDGRID*
- ************************************/
-/* API KEY */
-//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
-
-const transporter = nodemailer.createTransport(
-  sendgridTransport({
-    auth: {
-      api_key:
-        "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",
-    },
-  })
-);
+const sgMail = require("@sendgrid/mail");
+const { sendgridApiKey, activationTokenSecret } = require("../config/keys");
+sgMail.setApiKey(sendgridApiKey);
 
 /************************************
  *SIGNUP CONTROLLER
  ************************************/
-
 exports.signupController = async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
 
@@ -37,51 +21,89 @@ exports.signupController = async (req, res) => {
       });
     }
 
-    const newGuest = new Guest();
-    newGuest.firstname = firstname;
-    newGuest.lastname = lastname;
-    newGuest.email = email;
-
     const salt = await bcrypt.genSalt(10);
-    newGuest.password = await bcrypt.hash(password, salt);
+    hashPassword = await bcrypt.hash(password, salt);
 
-    await newGuest
-      .save()
+    const newGuest = {
+      firstname,
+      lastname,
+      email,
+      password: hashPassword,
+    };
 
-      /************************************
-       *SENDGRID*
-       ************************************/
-      .then((guest) => {
-        transporter.sendMail({
-          to: guest.email,
-          from: "team.boardmein@gmail.com",
-          subject: "Signup Success",
-          html: "<h1> Welcome to BoardMeIn Family</h1>",
-        });
-      });
+    const activation_token = createActivationToken(newGuest);
+
+    const msg = {
+      from: "bhnsandu69@gmail.com",
+      to: newGuest.email,
+      subject: "Board Me In - verify your email",
+      text: `
+          Hello, Thanks for registering on Board-Me-In.
+          Please copy and paste the address below to verify your account.
+          http://localhost:3000/guest/emailverify/${activation_token}`,
+      html: `<h1>Hello ${newGuest.firstname},</h1>
+          <p>Thanks for registering on Board-Me-In.</p>
+          <p>Please click the below link to verify your account.</p>
+          <a href="http://localhost:3000/guest/emailverify/${activation_token}"> Verify My Account </a>
+          `,
+    };
 
     //sending the response to frontend
     res.json({
-      successMessage: "Registration success. Please Login.",
+      successMessage:
+        "Thank You for Registering on BoardMeIn! We have sent a verification link to your email address provided. Please click on the link to verify your account. Please note that the verification link will expire within 10 minutes. ",
     });
+
+    //sending the mail
+    try {
+      await sgMail.send(msg);
+    } catch (error) {
+      console.log(error);
+    }
   } catch (err) {
     console.log("signupController error: ", err);
     res.status(500).json({
       errorMessage: "Server error",
     });
-
-    // console.log(newGuest.password);
-    //   } catch (err) {
-    //     console.log("signupController error:", err);
-    //   }
   }
 };
-// };
+
+exports.activateEmail = async (req, res) => {
+  try {
+    const { activation_token } = req.body;
+    const user = jwt.verify(activation_token, activationTokenSecret);
+
+    const { firstname, lastname, email, password, isverified } = user;
+
+    const check = await Guest.findOne({ email });
+    if (check)
+      return res.status(400).json({
+        errorMessage:
+          "You have already activated your account!, Please Login using your email and password. ",
+      });
+
+    const newGuest = new Guest({
+      firstname,
+      lastname,
+      email,
+      password,
+      isverified,
+    });
+
+    await newGuest.save();
+
+    res.json({
+      successMessage:
+        "Your account has been successfully activated!, Please Login using your email and password.",
+    });
+  } catch (err) {
+    return res.status(500).json({ errorMessage: err.message });
+  }
+};
 
 /************************************
  *LOGIN CONTROLLER
  ************************************/
-
 exports.loginController = async (req, res) => {
   const { email, password } = req.body;
 
@@ -122,4 +144,10 @@ exports.loginController = async (req, res) => {
       errorMessage: "Server error",
     });
   }
+};
+
+const createActivationToken = (payload) => {
+  return jwt.sign(payload, activationTokenSecret, {
+    expiresIn: "60m",
+  });
 };
