93d1595c1284028dcb5f4d3bd58f7939ce7d0786
nuwani-attanayaka <73015740+nuwani-attanayaka@users.noreply.github.com>
2021-05-28 23:16:19 +0530

    added email verification function to owner

--- controllers/ownerAuth.js
+++ controllers/ownerAuth.js
@@ -1,7 +1,14 @@
 const Owner = require("../models/Owner");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { jwtSecret, jwtExpire } = require("../config/keys");
+const {
+  jwtSecret,
+  jwtExpire,
+  sendgridApiKey,
+  activationTokenSecret,
+} = require("../config/keys");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(sendgridApiKey);
 
 /************************************
  *SIGNUP CONTROLLER
@@ -17,27 +24,91 @@ exports.ownerSignupController = async (req, res) => {
       });
     }
 
-    const newOwner = new Owner();
-    newOwner.firstname = firstname;
-    newOwner.lastname = lastname;
-    newOwner.email = email;
-
     const salt = await bcrypt.genSalt(10);
-    newOwner.password = await bcrypt.hash(password, salt);
+    hashPassword = await bcrypt.hash(password, salt);
 
-    await newOwner.save();
+    const newOwner = {
+      firstname,
+      lastname,
+      email,
+      password: hashPassword,
+    };
+
+    const activation_token = createActivationToken(newOwner);
+
+    const msg = {
+      from: "team.boardmein@gmail.com",
+      to: newOwner.email,
+      subject: "Board Me In - verify your email",
+      text: `
+          Hello, Thanks for registering on Board-Me-In.
+          Please copy and paste the address below to verify your account.
+          http://localhost:3000/owner/oemailverify/${activation_token}`,
+      html: `<h1>Hello ${newOwner.firstname},</h1>
+          <p>Thank You for registering on Board-Me-In.</p>
+          <p>Please click the below link to verify your account.</p>
+          <a href="http://localhost:3000/owner/oemailverify/${activation_token}"> Verify My Account </a>
+          `,
+    };
+    // const newOwner = new Owner();
+    // newOwner.firstname = firstname;
+    // newOwner.lastname = lastname;
+    // newOwner.email = email;
+
+    // const salt = await bcrypt.genSalt(10);
+    // newOwner.password = await bcrypt.hash(password, salt);
+
+    // await newOwner.save();
 
     res.json({
-      successMessage: "Registration success. Please Login.",
+      successMessage:
+        "Thank You for Registering on BoardMeIn! We have sent a verification link to your email address provided. Please click on the link to verify your account. Please note that the verification link will expire within 10 minutes. ",
     });
+    //sending the mail
+    try {
+      await sgMail.send(msg);
+    } catch (error) {
+      console.log(error);
+    }
   } catch (err) {
-    console.log("ownerSignupController error: ", err);
+    console.log("signupController error: ", err);
     res.status(500).json({
       errorMessage: "Server error",
     });
   }
 };
 
+exports.activateEmail = async (req, res) => {
+  try {
+    const { activation_token } = req.body;
+    const user = jwt.verify(activation_token, activationTokenSecret);
+
+    const { firstname, lastname, email, password } = user;
+
+    const check = await Owner.findOne({ email });
+    if (check)
+      return res.status(400).json({
+        errorMessage:
+          "You have already activated your account!, Please Login using your email and password. ",
+      });
+
+    const newOwner = new Owner({
+      firstname,
+      lastname,
+      email,
+      password,
+    });
+
+    await newOwner.save();
+
+    res.json({
+      successMessage:
+        "Your account has been successfully activated!, Please Login using your email and password.",
+    });
+  } catch (err) {
+    return res.status(500).json({ errorMessage: err.message });
+  }
+};
 /************************************
  *LOGIN CONTROLLER
  ************************************/
@@ -81,4 +152,10 @@ exports.ownerLoginController = async (req, res) => {
       errorMessage: "Server error",
     });
   }
+};
+
+const createActivationToken = (payload) => {
+  return jwt.sign(payload, activationTokenSecret, {
+    expiresIn: "60m",
+  });
 };
